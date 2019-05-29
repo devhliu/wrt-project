bdafa41787e9f6757cc65b8ded9266b084531737
Fedor <fedor.goncharov.ol@gmail.com>
2019-05-29 23:12:58 +0200

    Delete GenCutoffFunc.m

--- wradon_inversion/iterative/GenCutoffFunc.m
+++
@@ -1,9 +0,0 @@
-% characteristic function of a unit ball in the unit cube in 3D
-
-function cutoff = GenCutoffFunc(ngrid)
-  
-  lin = linspace(-1, 1, ngrid);
-  [X,Y,Z] = meshgrid(lin, lin, lin);
-  cuttoff = ((X.^2 + Y.^2 + Z.^2)< 1);
-  
-endfunction
