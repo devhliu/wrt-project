fd768e9b38471b9fc075a2c3a13b4fd5ae687493
Fedor <fedor.goncharov.ol@gmail.com>
2019-06-09 16:40:58 +0200

    update makefile, stylistic corrections of the code

---
+++ radon_grid/init.h
@@ -0,0 +1,31 @@
+#ifndef __H_RADON_INIT
+#define __H_RADON_INIT
+
+/* Grid parameters utilites */
+
+int parameters_alloc(int** parameters);
+int parameters_init(char* filename, int* parameters);
+int parameters_clean(int* parameters);
+
+/* Initial values utilites */
+
+int tfunction_values_alloc(int ngrid, double**** values);
+int tfunction_values_init(char* filename, int ngrid, double*** values);
+int tfunction_values_clean(int ngrid, double*** values);
+
+/* Radon grid utilites */
+
+int radon_grid_alloc(int nphi, int ntheta, int nshift, double** phi, double** theta, double** shift);
+int radon_grid_init(int nphi, int ntheta, int nshift, double* phi, double* theta, double* shift);
+int radon_grid_clean(double* phi, double* theta, double* shift);
+
+
+/* Threads filenames utilites */
+
+int chunks_filenames_alloc(char* filename, int nchunks, char*** nchunks_filenames, int size);
+int chunks_filenames_init(char* filename, int nchunks, char** nchunks_filenames);
+void chunks_aggregate(char* output_filename, char ** chunks_filenames, int nchunks);
+int chunks_files_clean(int nthreads, char** chunks_filenames);
+int chunks_filenames_clean(int nchunks, char** nchunks_filenames);
+
+#endif
---
+++ radon_grid/integral.h
@@ -0,0 +1,7 @@
+#ifndef __H_RADON_INTEGRALS
+#define __H_RADON_INTEGRALS
+
+double plane_integral(double*** values, int ngrid, 
+			double phi, double theta, double shift);
+
+#endif
